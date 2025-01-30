41b5d423a1937847277126fa3be255227237135d
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-01-30 22:33:05 +0330

    ⚙️ tower of hanoi function

---
+++ hanoi.py
@@ -0,0 +1,13 @@
+def tower_of_hanoi(desired_loops, starting_point, auxiliary, destination):
+
+    if desired_loops == 1:
+        print(f"{starting_point}>{destination}")
+        return
+
+    tower_of_hanoi(desired_loops - 1, starting_point, destination, auxiliary)
+    print(f"{starting_point}>{destination}")
+    tower_of_hanoi(desired_loops - 1, auxiliary, starting_point, destination)
+
+
+given_number = int(input())
+tower_of_hanoi(given_number, "A", "B", "C")
