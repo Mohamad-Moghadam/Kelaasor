892ce0ec3c619aea35bca2dfaa4abe6a789f3fb4
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-01 23:21:44 +0330

    🚀 Completed the project

--- common_names.py
+++ common_names.py
@@ -8,17 +8,20 @@ def most_common_names():
     for count in range(num):
         name_sex = input().split()
         name_list.append(name_sex)
-    male = [name for name, gender in name_list if gender == "m"]
-    female = [name for name, gender in name_list if gender == "f"]
+    male = [name for name, gender in name_list if gender == "M"]
+    female = [name for name, gender in name_list if gender == "F"]
     ordered_list_m = sorted(male)
     ordered_list_f = sorted(female)
     m_counter = Counter(ordered_list_m).most_common(1)
     f_counter = Counter(ordered_list_f).most_common(1)
 
-    most_common_males = m_counter[0] if m_counter else None
-    most_common_females = f_counter[0] if f_counter else None
+    most_common_males = m_counter[0][0] if m_counter else None
+    most_common_females = f_counter[0][0] if f_counter else None
 
-    pprint(most_common_males, most_common_females)
+    if most_common_males:
+        print(most_common_females)
+    if most_common_females:
+        print(most_common_males)
 
 
 most_common_names()
