b4bdff7aef82819b256553f3faa339af78750df9
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-02 22:06:09 +0330

    🐛 some bugs were fixed

--- only_once.py
+++ only_once.py
@@ -2,11 +2,10 @@ def only_once(a, b):
     final_list = list(zip(a, b))
     filtered_list = [item for item in final_list if item[0] != item[1]]
     if not filtered_list:
-        return "YES"
+        print("YES")
+        return
     differences = [abs(item[0] - item[1]) for item in filtered_list]
-    for i in range(len(differences) - 1):
-        final_difference = differences[i] - differences[i + 1]
-    if final_difference == 0:
+    if all(diff == differences[0] for diff in differences):
         print("YES")
     else:
         print("NO")
