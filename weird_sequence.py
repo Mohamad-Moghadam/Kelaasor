af49d0bbd03386c47fe42037b37e7d99f81fe820
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-02 20:08:58 +0330

    🚧 rewriting the code for better perf

--- weird_sequence.py
+++ weird_sequence.py
@@ -1,8 +1,10 @@
 def weird_sequence(desired_number):
-    sequence = [10**n for n in range(desired_number)]
-    final_sequence = "".join(map(str, sequence))
-    the_number = final_sequence[desired_number - 1]
-    print(the_number)
+    final_sequence = ""
+    n = 0
+    while len(final_sequence) < desired_number:
+        final_sequence += str(10**n)
+        n += 1
+    print(final_sequence[desired_number - 1])
 
 
 desired_number = int(input())
