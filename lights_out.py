e4020d52306b7192fda2615166e6f0ab90de2e84
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-02 16:54:51 +0330

    🔧 used for loops for items in each list

--- lights_out.py
+++ lights_out.py
@@ -1,8 +1,37 @@
 def lights_out(a, b, c):
+    row1 = a[:]
+    row2 = b[:]
+    row3 = c[:]
 
-    added1 = [(index + 1) % 2 for index in a if index > 0]
-    added2 = [(index + 1) % 2 for index in b if index > 0]
-    added3 = [(index + 1) % 2 for index in c if index > 0]
+    for i in range(3):
+        if row1[i] == 1:
+            if i > 0:
+                row1[i - 1] = 1 - row1[i - 1]
+            row1[i] = 1 - row1[i]
+            if i < 2:
+                row1[i + 1] = 1 - row1[i + 1]
+            row2[i] = 1 - row2[i]
+
+        if row2[i] == 1:
+            if i > 0:
+                row2[i - 1] = 1 - row2[i - 1]
+            row2[i] = 1 - row2[i]
+            if i < 2:
+                row2[i + 1] = 1 - row2[i + 1]
+            row1[i] = 1 - row1[i]
+            row3[i] = 1 - row3[i]
+
+        if row3[i] == 1:
+            if i > 0:
+                row3[i - 1] = 1 - row3[i - 1]
+            row3[i] = 1 - row3[i]
+            if i < 2:
+                row3[i + 1] = 1 - row3[i + 1]
+            row2[i] = 1 - row2[i]
+
+    on_off_row1 = [(item % 2) for item in row1]
+    on_off_row2 = [(item % 2) for item in row2]
+    on_off_row3 = [(item % 2) for item in row3]
 
 
 line1 = list(map(int, input().split()))
