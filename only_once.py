9f51a982c938a119c30160437a3e14390626c9e4
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-02 18:59:29 +0330

    🚧 still working on it

--- only_once.py
+++ only_once.py
@@ -1,15 +1,13 @@
 def only_once(a, b):
     final_list = list(zip(a, b))
     filtered_list = [item for item in final_list if item[0] != item[1]]
-    for i in range(len(filtered_list) - 1):
-        difference = abs(filtered_list[i][0] - filtered_list[i][1])
-        next_difference = abs(filtered_list[i + 1][0] - filtered_list[i + 1][1])
-        if next_difference == difference + 1:
-            print("Yes")
-        else:
-            print("No")
+    differences = [abs(item[0] - item[1]) for item in filtered_list]
+    for i in range(len(differences) - 1):
+        if differences[i] == differences[i + 1] - 1:
+            return "Yes"
+    return "No"
 
 
 first_list = list(map(int, input().split()))
 second_list = list(map(int, input().split()))
-only_once(first_list, second_list)
+print(only_once(first_list, second_list))
