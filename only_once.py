6c8902d51763daec538e8d667afdd7cf271df518
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-02 19:50:47 +0330

    🚧 everything is completed except one thing

--- only_once.py
+++ only_once.py
@@ -1,11 +1,14 @@
 def only_once(a, b):
     final_list = list(zip(a, b))
     filtered_list = [item for item in final_list if item[0] != item[1]]
+    if not filtered_list:
+        return "Yes"
     differences = [abs(item[0] - item[1]) for item in filtered_list]
     for i in range(len(differences) - 1):
-        if differences[i] == differences[i + 1] - 1:
+        if differences[i] != differences[i + 1]:
+            return "No"
+        else:
             return "Yes"
-    return "No"
 
 
 first_list = list(map(int, input().split()))
