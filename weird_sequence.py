8bbcc75389160efa0aa5505a364a140b249834a3
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-02 20:12:47 +0330

    🎉 project completed

--- weird_sequence.py
+++ weird_sequence.py
@@ -4,6 +4,7 @@ def weird_sequence(desired_number):
     while len(final_sequence) < desired_number:
         final_sequence += str(10**counter)
         counter += 1
+    print(final_sequence[desired_number - 1])
 
 
 desired_number = int(input())
