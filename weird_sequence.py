48cfe9165c0677ec3191a98a6b89759d232aea6b
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-02 20:11:06 +0330

    🚀 used while for better perf

--- weird_sequence.py
+++ weird_sequence.py
@@ -1,10 +1,9 @@
 def weird_sequence(desired_number):
     final_sequence = ""
-    n = 0
+    counter = 0
     while len(final_sequence) < desired_number:
-        final_sequence += str(10**n)
-        n += 1
-    print(final_sequence[desired_number - 1])
+        final_sequence += str(10**counter)
+        counter += 1
 
 
 desired_number = int(input())
