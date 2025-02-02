45f324e9f3547a2a335085a60d6c12b4af1f1f44
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-02 20:18:25 +0330

    🚀 faster run and use of list and .extend

--- weird_sequence.py
+++ weird_sequence.py
@@ -1,11 +1,11 @@
 def weird_sequence(desired_number):
-    final_sequence = ""
+    final_sequence = []
     counter = 0
     while len(final_sequence) < desired_number:
-        final_sequence += str(10**counter)
+        final_sequence.extend(str(10**counter))
         counter += 1
-    print(final_sequence[desired_number - 1])
+    return final_sequence[desired_number - 1]
 
 
 desired_number = int(input())
-weird_sequence(desired_number)
+print(weird_sequence(desired_number))
