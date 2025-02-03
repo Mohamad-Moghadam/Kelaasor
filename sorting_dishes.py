eab34928ee9182c342341c56ee38545765f41111
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-03 10:36:53 +0330

    🐛 some bugs are fixed

--- sorting_dishes.py
+++ sorting_dishes.py
@@ -1,17 +1,45 @@
+from collections import Counter
+
+
 def sort(d):
-    zero_list = []
+    zero_list_beginning = []
+    if d[0] == 0:
+        i = 0
+        while i < len(d) and d[i] == 0:
+            zero_list_beginning.append(i)
+            i += 1
+
+    zero_list_middle = []
+    starring_point = None
     for i in range(len(d)):
-        if (
-            (d[i] == 0)
-            and (d[i + 1] == 0 or d[i + 1] == 1)
-            and (d[i - 1] == 1 or d[i - 1] == 0)
-        ):
-            zero_list.append(i)
-    print(len(zero_list))
-    # zero_list = [i for i in d if i == 0]
-    # ones_list = [i for i in d if i == 1]
-    # combination = len(zero_list) + len(ones_list)
-    # print(combination)
+        if d[i] == 1:
+            starring_point = i + 1
+            break
+    if starring_point is not None:
+        for i in range(starring_point, len(d)):
+            while i < len(d) and d[i] == 0:
+                zero_list_middle.append(i)
+
+    zero_list_end = []
+    if d[-1] == 0:
+        i = len(d) - 1
+        while i >= 0 and d[i] == 0:
+            zero_list_end.append(i)
+            i -= 1
+
+    z1 = len(zero_list_beginning)
+    z2 = len(zero_list_middle)
+    z3 = len(zero_list_end)
+
+    zero = max(z1, z2, z3)
+
+    ones_list = [i for i in d if i == 1]
+    print(zero)
+    print(zero_list_beginning)
+    print(zero_list_middle)
+    print(zero_list_middle)
+    combination = len(ones_list) + zero
+    print(combination)
 
 
 the_dishes = list(map(int, input().split()))
