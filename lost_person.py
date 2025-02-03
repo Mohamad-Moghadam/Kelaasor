0ec71bb5b8c0d2cfe748dda4db0641cd67ff0969
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-03 22:47:46 +0330

    🎉 project completed

---
+++ lost_person.py
@@ -0,0 +1,12 @@
+def lost_person(num):
+    if num[0] == 1:
+        for i in range(len(num)):
+            if i > 0 and num[i] - num[i - 1] != 1:
+                print(num[i - 1] + 1)
+    else:
+        print(1)
+
+
+numbers = list(map(int, input().split()))
+sorted_nums = sorted(numbers)
+lost_person(sorted_nums)
