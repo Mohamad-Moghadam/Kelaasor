da5925d1e38c40029df1c100291f2684de0a9cdb
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-04 23:24:12 +0330

    🚧 small and medium shirts completed

---
+++ kelaasor_tshirt.py
@@ -0,0 +1,11 @@
+def T_shirt(wanted_shirts):
+
+    return
+
+
+size = []
+s_m_l_xl_xxl = input().split()
+num_of_people = int(input())
+for i in range(num_of_people):
+    size.append(input().split())
+T_shirt(s_m_l_xl_xxl)
