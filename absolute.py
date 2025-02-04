ad67fad55cbac63989aef1eae9b64fe03b9d504e
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-05 01:48:03 +0330

    🚧 still need to give more outputs

--- absolute.py
+++ absolute.py
@@ -1,22 +1,23 @@
 def absolute(num):
     c = []
     start = 0
-    for i in range(len(num)):
+    for i in range(len(num) - 1):
         for j in range(len(num[i])):
             if num[i][j] >= 0:
                 start += num[i][j]
                 c.append(start)
-                if j == num[-1]:
-                    start = 0
-                    break
+            elif num[i][j] < 0 and num[i][j + 1] < 0:
+                num[i][j] += num[i][j + 1]
+                start += num[i][j]
+                if start < 0:
+                    start = -start
+                    c.append(start)
             elif num[i][j] < 0:
                 start += num[i][j]
                 if start < 0:
                     start = -start
                     c.append(start)
-                    if j == num[-1]:
-                        start = 0
-                        break
+        start = 0
     print(start)
     print(c)
 
