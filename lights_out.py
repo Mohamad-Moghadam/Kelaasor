c008564040d522169f107842cbf2c3e8def3759b
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-04 20:26:04 +0330

    🎉 tried a new way and completed

--- lights_out.py
+++ lights_out.py
@@ -1,40 +1,53 @@
 def lights_out(a, b, c):
+
     row1 = a[:]
     row2 = b[:]
     row3 = c[:]
 
-    for i in range(3):
-        if row1[i] == 1:
-            if i > 0:
-                row1[i - 1] = 1 - row1[i - 1]
-            row1[i] = 1 - row1[i]
-            if i < 2:
-                row1[i + 1] = 1 - row1[i + 1]
-            row2[i] = 1 - row2[i]
+    if a[0] > 0:
+        row1[1] += a[0]
+        row2[0] += a[0]
+    if a[1] > 0:
+        row1[0] += a[1]
+        row1[2] += a[1]
+    if a[2] > 0:
+        row1[1] += a[2]
+        row2[2] += a[2]
+    if b[0] > 0:
+        row1[0] += b[0]
+        row2[1] += b[0]
+        row3[0] += b[0]
+    if b[1] > 0:
+        row2[0] += b[1]
+        row1[1] += b[1]
+        row3[1] += b[1]
+        row2[2] += b[1]
+    if b[2] > 0:
+        row2[1] += b[2]
+        row1[2] += b[2]
+        row3[2] += b[2]
+    if c[0] > 0:
+        row2[0] += c[0]
+        row3[1] += c[0]
+    if c[1] > 0:
+        row3[0] += c[1]
+        row2[1] += c[1]
+        row3[2] += c[1]
+    if c[2] > 0:
+        row3[1] += c[2]
+        row2[2] += c[2]
 
-        if row2[i] == 1:
-            if i > 0:
-                row2[i - 1] = 1 - row2[i - 1]
-            row2[i] = 1 - row2[i]
-            if i < 2:
-                row2[i + 1] = 1 - row2[i + 1]
-            row1[i] = 1 - row1[i]
-            row3[i] = 1 - row3[i]
+    on_off_row1 = list(map(str, [((item + 1) % 2) for item in row1]))
+    on_off_row2 = list(map(str, [((item + 1) % 2) for item in row2]))
+    on_off_row3 = list(map(str, [((item + 1) % 2) for item in row3]))
 
-        if row3[i] == 1:
-            if i > 0:
-                row3[i - 1] = 1 - row3[i - 1]
-            row3[i] = 1 - row3[i]
-            if i < 2:
-                row3[i + 1] = 1 - row3[i + 1]
-            row2[i] = 1 - row2[i]
-
-    on_off_row1 = [(item % 2) for item in row1]
-    on_off_row2 = [(item % 2) for item in row2]
-    on_off_row3 = [(item % 2) for item in row3]
+    print(" ".join(on_off_row1))
+    print(" ".join(on_off_row2))
+    print(" ".join(on_off_row3))
 
 
 line1 = list(map(int, input().split()))
 line2 = list(map(int, input().split()))
 line3 = list(map(int, input().split()))
+
 lights_out(line1, line2, line3)
