b35af6bafc54a0a26b20dde4611ffbdba44cd568
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-05 00:22:31 +0330

    🐛 not using lists inside the list

--- kelaasor_tshirt.py
+++ kelaasor_tshirt.py
@@ -1,58 +1,59 @@
 def T_shirt(wanted_shirts, size):
+    print(wanted_shirts, size)
     given_shirts = []
-    for item in size:
-        if item == "S" and wanted_shirts[0] > 0:
+    for item in range(len(size)):
+        if size[item] == "S" and wanted_shirts[0] > 0:
             given_shirts.append("S")
             wanted_shirts[0] -= 1
-        elif item == "S" and wanted_shirts[1] > 0:
+        elif size[item] == "S" and wanted_shirts[1] > 0:
             given_shirts.append("M")
             wanted_shirts[1] -= 1
-        elif item == "S" and wanted_shirts[2] > 0:
+        elif size[item] == "S" and wanted_shirts[2] > 0:
             given_shirts.append("L")
             wanted_shirts[2] -= 1
         else:
             given_shirts.append("No Shirt")
             continue
-        if item == "M" and wanted_shirts[1] > 0:
+        if size[item] == "M" and wanted_shirts[1] > 0:
             given_shirts.append("M")
             wanted_shirts[1] -= 1
-        elif item == "M" and wanted_shirts[2] > 0:
+        elif size[item] == "M" and wanted_shirts[2] > 0:
             given_shirts.append("L")
             wanted_shirts[2] -= 1
-        elif item == "M" and wanted_shirts[0] > 0:
+        elif size[item] == "M" and wanted_shirts[0] > 0:
             given_shirts.append("S")
             wanted_shirts[0] -= 1
-        elif item == "M" and wanted_shirts[3] > 0:
+        elif size[item] == "M" and wanted_shirts[3] > 0:
             given_shirts.append("XL")
             wanted_shirts[3] -= 1
         else:
             given_shirts.append("No Shirt")
             continue
-        if item == "L" and wanted_shirts[2] > 0:
+        if size[item] == "L" and wanted_shirts[2] > 0:
             given_shirts.append("L")
             wanted_shirts[2] -= 1
-        elif item == "L" and wanted_shirts[3] > 0:
+        elif size[item] == "L" and wanted_shirts[3] > 0:
             given_shirts.append("XL")
             wanted_shirts[3] -= 1
-        elif item == "L" and wanted_shirts[1] > 0:
+        elif size[item] == "L" and wanted_shirts[1] > 0:
             given_shirts.append("M")
             wanted_shirts[1] -= 1
-        elif item == "L" and wanted_shirts[4] > 0:
+        elif size[item] == "L" and wanted_shirts[4] > 0:
             given_shirts.append("XXL")
             wanted_shirts[4] -= 1
-        elif item == "L" and wanted_shirts[0] > 0:
+        elif size[item] == "L" and wanted_shirts[0] > 0:
             given_shirts.append("S")
             wanted_shirts[0] -= 1
         else:
             given_shirts.append("No Shirt")
             continue
-        if item == "XXL" and wanted_shirts[4] > 0:
+        if size[item] == "XXL" and wanted_shirts[4] > 0:
             given_shirts.append("XXL")
             wanted_shirts[4] -= 1
-        elif item == "XXL" and wanted_shirts[3] > 0:
+        elif size[item] == "XXL" and wanted_shirts[3] > 0:
             given_shirts.append("XL")
             wanted_shirts[3] -= 1
-        elif item == "XXL" and wanted_shirts[2] > 0:
+        elif size[item] == "XXL" and wanted_shirts[2] > 0:
             given_shirts.append("L")
             wanted_shirts[2] -= 1
         else:
@@ -65,6 +66,5 @@ size = []
 s_m_l_xl_xxl = list(map(int, input().split()))
 num_of_people = int(input())
 for i in range(num_of_people):
-    size.append(input().split())
-requests = "".join(size)
-T_shirt(s_m_l_xl_xxl, requests)
+    size.extend(input().split())
+T_shirt(s_m_l_xl_xxl, size)
