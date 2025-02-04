869827ac88ad2e55f41c3c8c34849424bf2a4839
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-04 19:21:45 +0330

    🚧 still a lot to do

--- messenger.py
+++ messenger.py
@@ -1,13 +1,19 @@
 def messenger(m):
-    i = 0
-    while i < len(m):
-        c = 1
-        while i + 1 < len(m) and m[i] == m[i + 1]:
-            c += 1
+    chars = []
+    c = 1
+    for i in range(len(m) - 1):
+        if m[i] != " ":
+            if m[i] == m[i + 1]:
+                c += 1
+            else:
+                chars.append(m[i] + str(c))
+                c = 1
+        else:
             i += 1
-        m.insert(i + 1, str(c))
+        chars.append(m[-1] + str(c))
 
-    print(" ".join(m))
+    final = "".join(chars)
+    print(chars)
 
 
 message = input().split()
