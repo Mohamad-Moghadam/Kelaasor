afec03ec612fa96f7c02bee62e355f17fb7eebea
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-04 13:16:32 +0330

    🐛 another 1st pair bug fixed

--- bracket.py
+++ bracket.py
@@ -1,11 +1,21 @@
 def bracket(b):
     for i in range(len(b)):
         if i == 0:
-            if b[0] == b[-1]:
-                print(True)
+            if ord(b[0]) == 40:
+                if ord(b[-1]) == 41:
+                    print(True)
+            elif ord(b[0]) == 91:
+                if ord(b[-1]) == 93:
+                    print(True)
+            elif ord(b[0]) == 123:
+                if ord(b[-1]) == 125:
+                    print(True)
             else:
                 print(False)
 
 
-brackets = input().split()
-bracket(brackets)
+brackets = input()
+new_brackets = " ".join(brackets)
+bracket_list = new_brackets.split()
+print(bracket_list)
+bracket(bracket_list)
