2311f8cbf82dddf816a9424e8d18d937bc6cf4a9
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-07 12:48:34 +0330

    🔨 writing an alternative for return

--- weird_sequence.py
+++ weird_sequence.py
@@ -1,6 +1,9 @@
 def weird_sequence(n):
     i = int(((1 + (1 + 8 * (n - 1)) ** 0.5) / 2))
-    return 1 if (i * (i - 1)) // 2 == (n - 1) else 0
+    if (i * (i - 1)) // 2 == (n - 1):
+        return 1
+    else:
+        return 0
 
 
 desired_number = int(input())
