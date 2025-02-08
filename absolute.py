bbd960d0b427fe126310bad38580762c7aab68f4
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-08 10:50:33 +0330

    ⚙️ trying an alternative way for better functionality

--- absolute.py
+++ absolute.py
@@ -1,7 +1,4 @@
-import copy
-
-
-def absolute(num, tree, first):
+"""def absolute(num, tree, first):
     if 2 ** len(first) == len(tree):
         return max(tree)
 
@@ -35,3 +32,30 @@ for given_numbers in lists:
 
 results = list(map(str, results))
 print("\n".join(results))
+"""
+
+
+def max_c_value(case):
+
+    results = []
+    if 2 ** len(case) == len(results):
+        return max(results)
+
+    c1 = 0
+    c2 = 0
+    for i in range(len(case)):
+        c1 = c1 + case[i]
+        c2 = abs(c2 + case[i])
+        results.append(c1)
+        results.append(c2)
+
+    print(results)
+
+
+num_list = int(input())
+num_list_final = []
+for i in range(num_list):
+    a = list(map(int, input().split()))
+    num_list_final.append(a)
+    result = max_c_value(num_list_final)
+    del num_list_final[:]
