3f6cb81afb2fe2b02043d1648fe20809a685a7ab
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-08 11:24:50 +0330

    🐛 out of range error fixed

--- absolute.py
+++ absolute.py
@@ -1,40 +1,3 @@
-"""def absolute(num, tree, first):
-    if 2 ** len(first) == len(tree):
-        return max(tree)
-
-    for i in range(len(num)):
-        current_num = num[i]
-        new_tree = []
-
-        for k in range(len(tree)):
-            add = tree[k] + current_num
-            absl = abs(tree[k] + current_num)
-
-            new_tree.append(add)
-            new_tree.append(absl)
-
-        new_num = num[:i] + num[i + 1 :]
-        return absolute(new_num, new_tree, first)
-
-
-num_of_lists = int(input())
-lists = []
-for _ in range(num_of_lists):
-    given_numbers = list(map(int, input().split()))
-    lists.append(given_numbers)
-
-tree = [0]
-results = []
-for given_numbers in lists:
-    first = given_numbers.copy()
-    result = absolute(given_numbers, tree, first)
-    results.append(result)
-
-results = list(map(str, results))
-print("\n".join(results))
-"""
-
-
 def max_c_value(case):
 
     results = []
@@ -44,11 +7,12 @@ def max_c_value(case):
     c1 = 0
     c2 = 0
     for i in range(len(case)):
-        c1 = c1 + case[i]
+        c1 += case[i]
         c2 = abs(c2 + case[i])
         results.append(c1)
         results.append(c2)
-
+        if 2**i != len(case):
+            del results[: (2**i) + 1]
     print(results)
 
 
@@ -56,6 +20,5 @@ num_list = int(input())
 num_list_final = []
 for i in range(num_list):
     a = list(map(int, input().split()))
-    num_list_final.append(a)
-    result = max_c_value(num_list_final)
+    result = max_c_value(a)
     del num_list_final[:]
