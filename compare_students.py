bc331ac62d77b44d3b5fbbc653e9917ab5e8ea51
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-15 20:53:42 +0330

    ✨ test cases added

--- compare_students.py
+++ compare_students.py
@@ -1,12 +1,20 @@
+from __future__ import annotations
+
+
 class Student:
-    def __init__(self, name, grade):
+    def __init__(self, name: str, grade: int):
         self.__name = name
         self.__grade = grade
 
-    def compare_grade(self, other_student):
-        if self.__grade > Student(other_student).__grade:
-            return f"{self.__name} has a higher grade. "
-        elif self.__grade < Student(other_student).__grade:
-            return f"{Student(other_student).__name} has a higher grade. "
+    def compare_grade(self, other_student: Student):
+        if self.__grade > other_student.__grade:
+            print(f"{self.__name} has a higher grade. ")
+        elif self.__grade < other_student.__grade:
+            print(f"{other_student.__name} has a higher grade. ")
         else:
-            return f"{self.__name} and {Student(other_student).__name} has the same grades. "
+            print(f"{self.__name} and {other_student.__name} has the same grades. ")
+
+
+s1 = Student("Alice", 90)
+s2 = Student("Bob", 91)
+s1.compare_grade(s2)  # Alice has a higher grade.
