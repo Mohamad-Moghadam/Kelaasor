69c76094bc1fc7889595c6abe961715efd23ae3d
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-16 14:15:04 +0330

    ⚙️ built a manual robot

---
+++ robot.py
@@ -0,0 +1,42 @@
+class Robot:
+    def __init__(self, m: int, n: int, i: int, j: int):
+        self.__sheet_horizontal = m
+        self.__sheet_vertical = n
+        self.__placement_horizontal = i
+        self.__placement_vertical = j
+
+    def move_up(self):
+        if self.__placement_vertical < self.__sheet_vertical:
+            self.__placement_horizontal += 1
+        else:
+            return f"The robot can't get any higher. "
+
+    def move_left(self):
+        if self.__placement_horizontal > 1:
+            self.__placement_horizontal += 1
+        else:
+            return f"The robot can't get to the left. "
+
+    def move_down(self):
+        if self.__placement_vertical > 1:
+            self.__placement_horizontal += 1
+        else:
+            return f"The robot can't get any lower. "
+
+    def move_right(self):
+        if self.__placement_vertical < self.__sheet_vertical:
+            self.__placement_horizontal += 1
+        else:
+            return f"The robot can't get any higher. "
+
+    def __str__(self):
+        return f"{self.__placement_horizontal}, {self.__placement_vertical}"
+
+
+r = Robot(5, 5, 2, 2)
+r.move_up()
+r.move_left()
+r.move_down()
+r.move_right()
+r.move_up()
+print(r)
