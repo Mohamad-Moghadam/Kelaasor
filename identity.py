6f6248efcf88dde5ce5c82fefaa51a5f2193f22f
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-17 17:53:30 +0330

    🐛 some matching the test cases and methods buga fixed

--- identity.py
+++ identity.py
@@ -4,21 +4,21 @@ import sys
 
 class User:
     def __init__(self, username, password):
-        self.__users = {}
         self.__name = username
         self.__password = password
-        self.__users[self.__name] = self.__password
 
     def hash_name(self):
         name_bytes = self.__name.encode("utf-8")
         self.__password = self.__hashed_name = hashlib.sha256(name_bytes).hexdigest()
 
     def authenticate(self, given_name, given_password):
-        if self.__users[self.__name] == self.__password.hash_name():
+        if (
+            self.__name == given_name
+            and self.__password.hash_name() == given_password.hash_name()
+        ):
             print(f"identity confirmed✅ ")
         else:
             print(f"wrong! ")
-            sys.exit()
 
 
 user_list = []
