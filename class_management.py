b2a7e6d215a57dee3db2a134433a1ed7142999d2
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-19 15:26:41 +0330

    🚧 adding the lesson class

--- class_management.py
+++ class_management.py
@@ -6,23 +6,42 @@ class Teacher:
         self.__name = input("the new name: ")
 
 class Student:
-    def __init__(self, name, grades: list):
+    def __init__(self, name, grades: list=[]):
         self.__name = name
         self.__grades = grades
 
     def student_changes(self, number: int):
         if number == 1:
             print(f"self.__name")
-            menu()
+            s_menu()
         elif number == 2:
             self.__name = input("the new name: ")
-            menu()
+            s_menu()
         elif number == 3:
             def add_grade(new_grade : int = 0):
                 self.__grades.append(new_grade)
-                menu()
+                s_menu()
         elif number == 4:
             for i in range(len(self.__grades)):
                 gpa = sum(self.__grades[i])
                 print(gpa)
-                menu()
+                s_menu()
+
+
+
+
+class Lesson:
+    def __init__(self, teacher: Teacher, students: list):
+        self.__teacher = teacher
+        self.__students = students
+
+    def change_teacher(self):
+        name = input("the new teacher: ")
+        self.__teacher = Teacher(name)
+
+    def add_student(self):
+        name = input("new student: ")
+        self.__students.append(Student(name))
+
+    def grade(self):
+        
