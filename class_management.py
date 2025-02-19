9d82314cb0441fd8b74316f6438a79d0af2fdeeb
mohamad_reza <m.reza.s.moghadam@gmail.com>
2025-02-19 08:21:17 +0330

    🚧 student class added

--- class_management.py
+++ class_management.py
@@ -4,3 +4,25 @@ class Teacher:
 
     def change_name(self):
         self.__name = input("the new name: ")
+
+class Student:
+    def __init__(self, name, grades: list):
+        self.__name = name
+        self.__grades = grades
+
+    def student_changes(self, number: int):
+        if number == 1:
+            print(f"self.__name")
+            menu()
+        elif number == 2:
+            self.__name = input("the new name: ")
+            menu()
+        elif number == 3:
+            def add_grade(new_grade : int = 0):
+                self.__grades.append(new_grade)
+                menu()
+        elif number == 4:
+            for i in range(len(self.__grades)):
+                gpa = sum(self.__grades[i])
+                print(gpa)
+                menu()
